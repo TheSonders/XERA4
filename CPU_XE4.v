5760d22fe114260a506c38c83a9ee910846628de
Prog_PICs <30624560+ProgPICs@users.noreply.github.com>
2019-10-13 04:00:15 +0200

    Add files via upload

--- CPU_XE4.v
+++ CPU_XE4.v
@@ -26,10 +26,15 @@ reg [14:0] PC=0;	//Program Counter
 reg [14:0] SP=0;	//Stack Pointer
 reg [14:0] VP=0;	//Video Pointer
 reg [14:0] UP=0;	//User Pointer
+reg [14:0] WP=0;	//User Pointer 2
 reg [7:0] F=0;		//Flags Register
 reg [7:0] A=0;		//Accumulator
 reg [7:0] B=0;		//User Register
 reg [7:0] C=0;		//User Register
+reg [7:0] D=0;		//User Register
+reg [7:0] E=0;		//User Register
+reg [7:0] G=0;		//User Register
+reg [7:0] H=0;		//User Register
 
 //Non user accessible
 reg [7:0] IR=8'h0;		//Instruction register
@@ -45,9 +50,23 @@ always @(negedge clk) begin
 	3'h2:
 		case (IR)
 			`LD_A_B:				begin A<=B;QC<=3'h0;end
+			`LD_B_A:				begin B<=A;QC<=3'h0;end
+			`LD_C_A:				begin C<=A;QC<=3'h0;end
+			`CLR_B:				begin B<=8'h0;QC<=3'h0;end
+			`CLR_C:				begin C<=8'h0;QC<=3'h0;end
+			`CLR_D:				begin D<=8'h0;QC<=3'h0;end
+			`LLSHIFT_BC:		begin {F[`Cf],B,C}<={B,C,1'h0};QC<=3'h0;end
+			`LLSHIFT_A:			begin {F[`Cf],A}<={A,1'h0};QC<=3'h0;end
+			`LD_UP_BC:			begin	UP<={B[6:0],C};QC<=3'h0;end
+			`LD_VP_BC:			begin	VP<={B[6:0],C};QC<=3'h0;end
+			`LD_BC_VP:			begin	{B,C}<={1'h0,VP};QC<=3'h0;end
 			`LD_A_8,
-			`LD_B_8,	
+			`LD_B_8,
+			`LD_D_8,
+			`LD_E_8,
+			`LD_G_8,
 			`LD_UP_15,	
+			`LD_WP_15,	
 			`LD_VP_15,	
 			`GOTO_15,	
 			`RJMP_8,	
@@ -56,25 +75,45 @@ always @(negedge clk) begin
 			`LD_UP_8,		
 			`LD_VP_8,
 			`LD_BC_16,	
-			`JMPZ_8,	
+			`JMPZ_8,
+			`JMPC_8,
+			`ADD_B_8,
+			`ADD_C_8,
+			`ADD_D_8,
+			`ADDC_B_8,
 			`LDINC_$VP$_8:		begin RAM_Add<=PC;we<=0;QC<=3'h3;end
+			`LDINC_A_$UP$,
 			`LD_A_$UP$:			begin RAM_Add<=UP;we<=0;QC<=3'h3;end
+			`LDINC_A_$WP$:		begin RAM_Add<=WP;we<=0;QC<=3'h3;end
 			`LD_A_$VP$:			begin Video_Add<=VP;Video_we<=0;QC<=3'h3;end
 			`DEC_A:				begin	A<=A-8'b1;F[`Zf]<=(A==8'h1);QC<=3'h0;end
+			`DEC_C:				begin	C<=C-8'b1;F[`Zf]<=(C==8'h1);QC<=3'h0;end
+			`DEC_E:				begin	E<=E-8'b1;F[`Zf]<=(E==8'h1);QC<=3'h0;end
+			`DEC_G:				begin	G<=G-8'b1;F[`Zf]<=(G==8'h1);QC<=3'h0;end
 			`INC_A:				begin	A<=A+8'b1;F[`Zf]<=(A==8'hFF);QC<=3'h0;end
 			`DEC_BC:				begin	{B,C}<={B,C}-16'b1;F[`Zf]<=({B,C}==16'h1);QC<=3'h0;end
 			`LDINC_$VP$_A:		begin	Video_Add<=VP;Video_Out<=A;Video_we<=1;QC<=3'h3;end
 			`LDINC_$VP$_B:		begin	Video_Add<=VP;Video_Out<=B;Video_we<=1;QC<=3'h3;end
+			`LDINC_$VP$_D:		begin	Video_Add<=VP;Video_Out<=D;Video_we<=1;QC<=3'h3;end
 			default:				QC<=3'h0; //`NOP:
 		endcase
 	3'h3:
 		case (IR)
 			`LD_A_8:				begin A<=RAM_In;PC<=PC+15'h1;QC<=3'h0;end
 			`LD_B_8:				begin B<=RAM_In;PC<=PC+15'h1;QC<=3'h0;end
+			`LD_D_8:				begin D<=RAM_In;PC<=PC+15'h1;QC<=3'h0;end
+			`LD_E_8:				begin E<=RAM_In;PC<=PC+15'h1;QC<=3'h0;end
+			`LD_G_8:				begin G<=RAM_In;PC<=PC+15'h1;QC<=3'h0;end
+			`ADD_B_8:			begin {F[`Cf],B}<=B+RAM_In;PC<=PC+15'h1;QC<=3'h0;end
+			`ADD_C_8:			begin {F[`Cf],C}<=C+RAM_In;PC<=PC+15'h1;QC<=3'h0;end
+			`ADD_D_8:			begin {F[`Cf],D}<=D+RAM_In;PC<=PC+15'h1;QC<=3'h0;end
+			`ADDC_B_8:			begin {F[`Cf],B}<=B+RAM_In+F[`Cf];PC<=PC+15'h1;QC<=3'h0;end
 			`LD_UP_15:			begin UP[7:0]<=RAM_In;PC<=PC+15'h1;QC<=3'h4;end
+			`LD_WP_15:			begin WP[7:0]<=RAM_In;PC<=PC+15'h1;QC<=3'h4;end
 			`LD_VP_15:			begin VP[7:0]<=RAM_In;PC<=PC+15'h1;QC<=3'h4;end
 			`GOTO_15:			begin PCt[7:0]<=RAM_In;PC<=PC+15'h1;QC<=3'h4;end
 			`JMPZ_8:				begin PC<=(F[`Zf])?PC+{{7{RAM_In[7]}},RAM_In}:PC+15'h1;QC<=3'h0;end
+			`JMPC_8:				begin PC<=(F[`Cf])?PC+{{7{RAM_In[7]}},RAM_In}:PC+15'h1;QC<=3'h0;end
 			`RJMP_8:				begin	PC<=PC+{{7{RAM_In[7]}},RAM_In};QC<=3'h0;end
 			`SUB_A_8:			begin {F[`Cf],A}<=A-RAM_In;PC<=PC+15'h1;QC<=3'h0;end
 			`CMP_A_8:			begin	{F[`Cf],PCt[7:0]}<=A-RAM_In;F[`Zf]<=(A==RAM_In);PC<=PC+15'h1;QC<=3'h0;end
@@ -82,15 +121,19 @@ always @(negedge clk) begin
 			`LD_VP_8,	
 			`LDINC_$VP$_8:		begin	PCt[7:0]<=RAM_In;PC<=PC+15'h1;Video_Add<=VP;QC<=3'h4;end
 			`LD_BC_16:			begin C<=RAM_In;PC<=PC+15'h1;QC<=3'h4;end
-			`LD_A_$UP$:			begin A<=RAM_In;QC<=3'h0;end
+			`LD_A_$UP$:			begin A<=RAM_In;F[`Zf]<=(RAM_In==8'h0);QC<=3'h0;end
+			`LDINC_A_$UP$:		begin A<=RAM_In;F[`Zf]<=(RAM_In==8'h0);UP<=UP+15'h1;QC<=3'h0;end
+			`LDINC_A_$WP$:		begin A<=RAM_In;F[`Zf]<=(RAM_In==8'h0);WP<=WP+15'h1;QC<=3'h0;end
 			`LD_A_$VP$:			begin A<=Video_In;QC<=3'h0;end
 			`LDINC_$VP$_A,
-			`LDINC_$VP$_B:		begin	VP<=VP+15'h1;Video_we<=0;QC<=3'h0;end
+			`LDINC_$VP$_B,
+			`LDINC_$VP$_D:		begin	VP<=VP+15'h1;Video_we<=0;QC<=3'h0;end
 			default:				QC<=3'h0; //`NOP:
 		endcase
 	3'h4:
 		case (IR)
-			`LD_UP_15,	
+			`LD_UP_15,
+			`LD_WP_15,	
 			`LD_VP_15,	
 			`GOTO_15,	
 			`LD_BC_16:			begin RAM_Add<=PC;we<=0;QC<=3'h5;end
@@ -102,6 +145,7 @@ always @(negedge clk) begin
 	3'h5:
 		case (IR)
 			`LD_UP_15:			begin UP[14:8]<=RAM_In[6:0];PC<=PC+15'h1;QC<=3'h0;end
+			`LD_WP_15:			begin WP[14:8]<=RAM_In[6:0];PC<=PC+15'h1;QC<=3'h0;end
 			`LD_VP_15:			begin VP[14:8]<=RAM_In[6:0];PC<=PC+15'h1;QC<=3'h0;end
 			`GOTO_15:			begin PCt[14:8]<=RAM_In[6:0];QC<=3'h6;end
 			`LD_BC_16:			begin B<=RAM_In;PC<=PC+15'h1;QC<=3'h0;end
--- opcodes.v
+++ opcodes.v
@@ -1,26 +1,63 @@
 //This file is also used by the XERA4 compiler,
 //ensures there's a copy of this file on the compliler path
 //OPCODES						//Cycles	//Description
-`define	NOP				8'H00	// 3
-`define	LD_A_B			8'H01	// 3		Register B to Register A
+`define	NOP				8'H00	//  3
+`define	LD_A_B			8'H01	//  3		Register B to Register A
 `define	LD_A_8			8'H02	//	4		8 bits literal to Register A
 `define	LD_B_8			8'H03	//	4		8 bits literal to Register B
-`define	LD_UP_15			8'H04	//	6		15bits literal to User Pointer
-`define	LD_VP_15			8'H05	//	6		15bits literal to Video Pointer
-`define	GOTO_15			8'H06	//	7		15bits absolute Jump
-`define	RJMP_8			8'H07	//	4		8bits signed Jump
-`define	SUB_A_8			8'H08	//	4		2comp' substract literal-A
-`define	LD_UP_8			8'H09	//	5		8bits literal to address pointed by UP
-`define	LD_VP_8			8'H0A	//	5		8bits literal to address pointed by VP
-`define	LD_BC_16			8'H0B	//	6		16bits literal to BC
-`define	LD_A_$UP$		8'H0C	//	4		Contents of UP to A
-`define	LD_A_$VP$		8'H0D	//	4		Contents of VP to A
-`define	DEC_A				8'H0E	//	3		Decrements register A
-`define	INC_A				8'H0F	//	3		Increments register A
-`define	DEC_BC			8'H10	//	3		Decrements pair BC
-`define	CMP_A_8			8'H11	//	4		Update flags with A-8bits literal
-`define	JMPZ_8			8'H12	//	4		8bits signed Jump if Flag Z
-`define	LDINC_$VP$_8	8'H13	//	5		8bits literal to address pointed by VP and increments VP
-`define	LDINC_$VP$_A	8'H14	//	4		Register A to address pointed by VP and increments VP
-`define	LDINC_$VP$_B	8'H15	//	4		Register B to address pointed by VP and increments VP
+`define	LD_UP_15		8'H04	//	6		15bits literal to User Pointer
+`define	LD_WP_15		8'H05	//	6		15bits literal to User Pointer 2
+`define	LD_VP_15		8'H06	//	6		15bits literal to Video Pointer
+`define	GOTO_15			8'H07	//	7		15bits absolute Jump
+`define	RJMP_8			8'H08	//	4		8bits signed Jump
+`define	SUB_A_8			8'H09	//	4		2comp' substract literal-A
+`define	LD_UP_8			8'H0A	//	5		8bits literal to address pointed by UP
+`define	LD_VP_8			8'H0B	//	5		8bits literal to address pointed by VP
+`define	LD_BC_16		8'H0C	//	6		16bits literal to BC
+`define	LD_A_$UP$		8'H0D	//	4		Contents of UP to A
+`define	LD_A_$VP$		8'H0E	//	4		Contents of VP to A
+`define	DEC_A			8'H0F	//	3		Decrements register A
+`define	INC_A			8'H10	//	3		Increments register A
+`define	DEC_BC			8'H11	//	3		Decrements pair BC
+`define	CMP_A_8			8'H12	//	4		Update flags with A-8bits literal
+`define	JMPZ_8			8'H13	//	4		8bits signed Jump if Flag Z
+`define	LDINC_$VP$_8	8'H14	//	5		8bits literal to address pointed by VP and increments VP
+`define	LDINC_$VP$_A	8'H15	//	4		Register A to address pointed by VP and increments VP
+`define	LDINC_$VP$_B	8'H16	//	4		Register B to address pointed by VP and increments VP
+`define	LDINC_A_$UP$	8'H17	//	4		Contents of UP to A and increments UP
+`define	LDINC_A_$WP$	8'H18	//	4		Contents of WP to A and increments WP
+`define	LD_B_A			8'H19	//  3		Register A to Register B
+`define	LD_C_A			8'H1A	//  3		Register A to Register C
+`define CLR_C			8'H1B	//	3		Clear Register C
+`define CLR_B			8'H1C	//	3		Clear Register B
+`define LLSHIFT_BC		8'H1D	//	3		Logical Left Shift pair BC
+`define ADD_C_8			8'H1E	//	4		Add C + 8 bits literal
+`define ADDC_B_8		8'H1F	//	4		Add B + 8 bits literal + carry flag
+`define LLSHIFT_A		8'H20	//	3		Logical Left register A
+`define	DEC_C			8'H21	//	3		Decrements register C
+`define	LD_D_8			8'H22	//	4		8 bits literal to Register D
+`define	LDINC_$VP$_D	8'H23	//	4		Register D to address pointed by VP and increments VP
+`define	LD_UP_BC		8'H24	//	3		15bits of pair BC to User Pointer
+`define	LD_E_8			8'H25	//	4		8 bits literal to Register D
+`define	JMPC_8			8'H26	//	4		8bits signed Jump if Flag C
+`define CLR_D			8'H27	//	3		Clear Register D
+`define ADD_D_8			8'H28	//	4		Add D + 8 bits literal
+`define	DEC_E			8'H29	//	3		Decrements register E
+`define	LD_BC_VP		8'H2A	//	3		Pointer VP to pair BC
+`define	LD_VP_BC		8'H2B	//	3		15bits of pair BC to Video Pointer
+`define	LD_G_8			8'H2C	//	4		8 bits literal to Register G
+`define	DEC_G			8'H2D	//	3		Decrements register G
+`define ADD_B_8			8'H2E	//	4		Add B + 8 bits literal
+
+// Pendientes de implementar
+//Carga de un registro a otro
+//Carga de un literal de 8 bits a un registro
+//Carga de un literal de 16 bits a un par de registros
+//Carga de un registro a la posición apuntada por un par de registros //incremento
+//Carga de un literal de 8 bits a la posición apuntada por un par de registros //incremento
+//Carga de la posición apuntada por un par de registros a un registro //incremento
+
+//Intercambio de 2 registros
+
+
 
